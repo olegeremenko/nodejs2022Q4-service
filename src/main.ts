1af57ae87c20e99001ebadde52d7aeb5b54fc7a8
Aleh Yaromenka <aleh.yaromenka@insurity.com>
2023-02-04 21:49:14 +0300

    feature: use env config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+  const port: number = +(process.env['PORT'] || 4000);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
